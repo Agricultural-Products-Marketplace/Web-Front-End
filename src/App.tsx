1d594604ac0e256c36ac8a7dba019f691ffb203f
yosefsahle <yosefsahle48@gmail.com>
2024-03-23 15:22:26 +0300

    home Page

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,42 @@ function App() {
       },
       ]}
       />
+      <ProductSlider 
+      title='Browse By Category'
+      slog='category'
+      products={[
+        {
+          productName: "",
+          productPrice: 0,
+          rating: 0,
+          img: "https://sweetphi.com/wp-content/uploads/2019/03/How-to-make-an-instagram-worthy-fruit-bowl-2-683x1024.jpg",
+          discount: 0,
+          categoryName:"Fruits"
+        }
+      ]}
+      />
+      <ProductSlider 
+      slog='This Month'
+      title='Most Viewed Products'
+      products={[
+        {
+          productName: "Ageda",
+          productPrice: 36,
+          rating: 3,
+          img: "https://sweetphi.com/wp-content/uploads/2019/03/How-to-make-an-instagram-worthy-fruit-bowl-2-683x1024.jpg",
+          discount: 2,
+          categoryName:"Foods"
+      },
+      {
+          productName: "Ageda",
+          productPrice: 36,
+          rating: 3,
+          img: "https://sweetphi.com/wp-content/uploads/2019/03/How-to-make-an-instagram-worthy-fruit-bowl-2-683x1024.jpg",
+          discount: 2,
+          categoryName:"Fruits"
+      },
+      ]}
+      />
       <div className="category-card-button-slider">
       </div>
       <Footer/>
