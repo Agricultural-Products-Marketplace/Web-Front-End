7f2fd4c2678161b6c347b649d48f9dbe0e160690
yosefsahle <yosefsahle48@gmail.com>
2024-03-25 18:14:25 +0300

    sign in signup form edit(required)

--- src/Pages/auth/signIn/index.tsx
+++ src/Pages/auth/signIn/index.tsx
@@ -23,26 +23,23 @@ function SignIn() {
                         <div className="signin-content-image">
                             <img src="https://media.istockphoto.com/id/1329735717/vector/happy-family-in-garden-with-a-basket-full-of-fresh-vegetables-cabbage-pepper-tomato-carrot.jpg?s=612x612&w=0&k=20&c=qRw7mOTt0p8tAibmK41lzFqWYZsN2KhuAapu7ETnNUE=" alt="" />
                         </div>
-                        <div className="signin-content-form">
+                        <form className="signin-content-form" method="post">
                             <p><strong>Sign In</strong><br />Login now to track all your expenses and income at a place</p>
                             <small>Error Message</small>
-                            <input type="text" placeholder="Email/Phone"/>
+                            <input type="text" placeholder="Email/Phone" required/>
                     
                             <div className="signin-form-password-input">
-                            <input type={activecreatepassword ? "text" : "password"} placeholder="Your Password"/>
+                            <input type={activecreatepassword ? "text" : "password"} placeholder="Your Password" required/>
                             <button onClick={handleClickcreatepassword}>
                                 {activecreatepassword ? <i className="fa fa-eye"></i> : <i className="fa fa-eye-slash"></i>}
                             </button>
                             </div>
                             <a href="#" className="signin-forget-password">Forget Password</a>
                             
-                            <a href="#" className="signin-form-button">Log In</a>
-                            <a href="#" className="signin-form-button-google">
-                            <i className="fa-brands fa-google"></i>
-                                Continue With Google</a>
+                            <button className="signin-form-button">Log In</button>
                             
                             <span>Don't Have an account? <Link to={'/signUp'}>Sign Up</Link></span>
-                        </div>
+                        </form>
                     </div>
             </div>
                
