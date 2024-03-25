fad7d848000097eff91585fa6c40b5fa94d919e0
yosefsahle <yosefsahle48@gmail.com>
2024-03-25 18:37:13 +0300

    wish list finishup

--- src/Pages/wishlist/index.tsx
+++ src/Pages/wishlist/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import './index.css';
 import WishlistCard from "../../card/wishlistCard";
+import Product from "../../card/product";
+import NavBar from "../../commen/navBar";
 
 
 
 
 function WishList() {
     return(
+        <>
+        <NavBar />
         <div className="wishlist">
             <div className="wishlist-title">
                 <h3>WishList ({40})</h3>
@@ -23,7 +27,27 @@ function WishList() {
                 },
                ]}
                />
-            </div>  
+               
+               <div className="wishlist-for-you">
+               <div className="wishlist-for-you-title">
+                <p><hr />Just For You</p>
+                <a href="#">See All</a>
+               </div>
+               <div className="wishlist-for-you-products">
+                <Product 
+                products={[{
+                    productName:"Rice",
+                    productPrice : 165,
+                    rating : 3,
+                    discount : 30,
+                    img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0Zc6uCIB6x5cmZewJ_g5ebZmbLBpcS2EBLWDuo_PjxQ&s",
+                    categoryName : ''
+                }]}
+                /> 
+               </div>
+               </div>
+            </div> 
+        </> 
     )
 }
 
