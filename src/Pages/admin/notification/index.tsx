b90ef420621fc754592f2e83f984af8722e48797
yosefsahle <yosefsahle48@gmail.com>
2024-04-30 20:02:37 +0300

    Admin product page

---
+++ src/Pages/admin/notification/index.tsx
@@ -0,0 +1,11 @@
+import AdminTopCard from "../../../card/admin/adminTopCard";
+
+function AdminNotification() {
+    return(
+        <div className="admin-page-main">
+            <AdminTopCard title="Notification"/>
+        </div>
+    )
+}
+
+export default AdminNotification;
--- src/Pages/admin/products/index.tsx
+++ src/Pages/admin/products/index.tsx
@@ -1,9 +1,23 @@
 import AdminTopCard from "../../../card/admin/adminTopCard";
+import './index.css';
 
 function Products() {
     return(
         <div className="admin-page-main">
             <AdminTopCard title="Products"/>
+            <div className="admin-page-main-products">
+                <div className="admin-page-main-products-title">
+                    <h3>Product List</h3>
+                    <div className="admin-page-main-products-title-actions">
+                        <div className="admin-page-main-products-title-actions-search">
+                            <input type="text" />
+                        </div>
+                        <button>Add Product</button>
+                        <button>More <i className="fa-solid fa-arrow-right"></i></button>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
