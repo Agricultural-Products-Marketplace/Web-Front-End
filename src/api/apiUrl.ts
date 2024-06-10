2c45d34c032f7e7b31861734f14bfda5c981feee
yosefsahle <yosefsahle48@gmail.com>
2024-06-10 14:04:13 +0300

    APIUrl

--- src/api/apiUrl.ts
+++ src/api/apiUrl.ts
@@ -1,3 +1,3 @@
-export const url = "http://192.168.137.178:8000/api/";
-// export const url = "http://127.0.0.1:8000/api/";
+// export const url = "http://192.168.137.178:8000/api/";
+export const url = "http://127.0.0.1:8000/api/";
 // export const url = "http://192.168.137.233:8000/api/";
